a5642b75d727c61a79b6835bf66da3ec6f0584ac
KooZuKi <patrick8chen@gmail.com>
2016-05-05 01:19:28 -0700

    adding images

--- js/main.js
+++ js/main.js
@@ -1,61 +1,75 @@
 var zodiac = [
 	{
-		sign: "aquarius"
+		sign: "aquarius",
+		fortune: "",
+		image: "img/aquarius.jpg",
+		a: "http://aquarius.bateleur.eu/"
 	},
 	{
 		sign: "aries",
 		fortune: "",
-		image: ""
+		image: "img/aries.jpg",
+		a: "http://aries.bateleur.eu/"
 	},
 	{
 		sign: "cancer",
 		fortune: "",
-		image: ""
+		image: "img/cancer.jpg",
+		a: "http://cancer.bateleur.eu/"
 	},
 	{
 		sign: "capricorn",
 		fortune: "",
-		image: ""
+		image: "img/capricorn.jpg",
+		a: "http://capricorn.bateleur.eu/"
 	},
 	{
 		sign: "gemini",
 		fortune: "",
-		image: ""
+		image: "img/gemini.jpg",
+		a: "http://gemini.bateleur.eu/"
 	},
 	{
 		sign: "leo",
 		fortune: "",
-		image: ""
+		image: "img/leo.jpg",
+		a: "http://leo.bateleur.eu/"
 	},
 	{
 		sign: "libra",
 		fortune: "",
-		image: ""
+		image: "img/libra.jpg",
+		a: "http://libra.bateleur.eu/"
 	},
 	{
 		sign: "pisces",
 		fortune: "",
-		image: ""
+		image: "img/pisces.jpg",
+		a: "http://pisces.bateleur.eu/"
 	},
 	{
 		sign: "sagittarius",
 		fortune: "",
-		image: ""
+		image: "img/sagittarius.jpg",
+		a: "http://sagittarius.bateleur.eu/"
 	},
 	{
 		sign: "scorpio",
 		fortune: "",
-		image: ""
+		image: "img/scorpio.jpg",
+		a: "http://scorpio.bateleur.eu/"
 	},
 	{
 		sign: "taurus",
 		fortune: "",
-		image: ""
+		image: "img/taurus.jpg",
+		a: "http://taurus.bateleur.eu/"
 	},
 	{
 		sign: "virgo",
 		fortune: "",
-		image: ""
+		image: "img/virgo.jpg",
+		a: "http://virgo.bateleur.eu/"
 	},
 ]
 function horoscope() {
@@ -64,12 +78,15 @@ function horoscope() {
 		if(sign === zodiac[i].sign) {
 			document.getElementById("yourSign").innerHTML = zodiac[i].sign
 			document.getElementById("icon").src = zodiac[i].image
+			document.getElementById("refer").href = zodiac[i].a
 			document.getElementById("yourHoroscope").innerHTML = "you're best attributes are: " + zodiac[i].fortune
 			return
 		} else {
 				document.getElementById("yourSign").innerHTML = "That's not one of the signs. Try again!"
 				document.getElementById("yourHoroscope").innerHTML = ""
+				document.getElementById("refer").href=""
 				document.getElementById("icon").src=""
+
 		}
 
 	}
